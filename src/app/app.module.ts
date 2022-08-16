33e4c62c5f9a27e65cb4d523d2b956e2c1616631
Simon Vargas <sjvargas01@gmail.com>
2022-08-16 08:44:35 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ExcelsheetComponent } from './excelsheet/excelsheet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExcelsheetComponent
   ],
   imports: [
     BrowserModule
